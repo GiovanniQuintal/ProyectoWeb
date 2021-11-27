b386bbe2cc8521de9cd4399a6296b3257e872347
GiovanniQuintal <g.mashiio97@gmail.com>
2021-11-27 03:05:57 -0600

    new

---
+++ Login.php
@@ -0,0 +1,63 @@
+<?php
+
+  session_start();
+
+  if (isset($_SESSION['user_id'])) {
+    header('Location: /ProyectoWeb3');
+  }
+
+  require 'database.php';
+
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    $records->bindParam(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      $_SESSION['user_id'] = $results['id'];
+      header("Location: /ProyectoWeb3");
+    } else {
+      $message = 'El email o password son erróneas';
+    }
+  }
+
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="css/estilosLoginRegistro.css" rel="stylesheet" type="text/css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- Header nav -->
+        <?php require "partials/header.php" ?>
+
+        <?php if(!empty($user)): ?>
+      <br> Hola. <?= $user['email']; ?>
+      <?php endif; ?>
+
+        <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+        <!-- Login -->
+            <h1>Inicia sesion</h1>
+            <form action="Login.php" method="POST">
+                <input name="email" type="text" placeholder="Email">
+                <input name="password" type="password" placeholder="Password">   
+                <input type="submit" class="button" value="Iniciar sesion">
+
+                <p>No tienes cuenta?&nbsp<a class="link" href="Registro.php">Registrate</a></p> 
+        </form>
+        <!-- JS de bootstrap para animaciones -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-black">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/ProyectoWeb2">
+                    <img src="imgs/logo.png" alt="" width="350">
+                </a>
+                
+                <button class="navbar-toggler" 
+                        type="button"
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="productos/tienda.php">Catálogo</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Noticias</a></li>
+                        <li class="nav-item"><a class="nav-link" href="Login.php">Inicia sesión</a></li>
+                        <li class="nav-item"><a class="nav-link" href="Registro.php">Regístrate</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+                        <li class="nav-item"><a class="nav-link" href="productos/productos.php">Agregar Producto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
