3963fd1cc45c9d09bb3b88815b4cd2f3fa0b5f38
EAragonS <eliasaragon15@gmail.com>
2021-11-30 23:41:40 -0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Login.php
    	new file:   agregarnot.php
    	new file:   config.php
    	new file:   contacto.php
    	modified:   index.php
    	new file:   noticias.php
    	modified:   partials/header.php

--- Login.php
+++ Login.php
@@ -1,11 +1,10 @@
 <?php
 
-session_start();
+  
 
   if (isset($_SESSION['user_id'])) {
-    header('Location: /ProyectoWeb3');
+    header('Location: index.php');
   }
-
   require 'database.php';
 
   if (!empty($_POST['email']) && !empty($_POST['password'])) {
@@ -18,7 +17,7 @@ session_start();
 
     if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
       $_SESSION['user_id'] = $results['id'];
-      header("Location: /ProyectoWeb3");
+      header("Location: index.php");
     } else {
       $message = 'El email o password son erróneas';
     }
@@ -35,7 +34,7 @@ session_start();
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="css/estilosLoginRegistro.css" rel="stylesheet" type="text/css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
+      </head>
     <body>
         <!-- Header nav -->
         <?php require "partials/header.php" ?>
@@ -54,7 +53,7 @@ session_start();
                 <input name="password" type="password" placeholder="Password">   
                 <input type="submit" class="button" value="Iniciar sesion">
 
-                <p>No tienes cuenta?&nbsp<a class="link" href="Registro.php">Registrate</a></p> 
+                <p>No tienes cuenta?&nbsp<a class="link" href="Registro.php">Registrate</a></p>
         </form>
         <!-- JS de bootstrap para animaciones -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
